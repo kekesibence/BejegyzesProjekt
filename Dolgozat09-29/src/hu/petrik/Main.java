1f43c7baa7a1b8833e4a94b1b35b52891f332804
kekesibence <bence.kekesi@gmail.com>
2021-09-29 16:16:23 +0200

    Bekérős feladat létrehozása Ellenőrző metódus létrehozása

--- Dolgozat09-29/src/hu/petrik/Main.java
+++ Dolgozat09-29/src/hu/petrik/Main.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Main {
 
     static List<Bejegyzes> Bejegyzesek = new ArrayList();
-    //static Scanner src = new Scanner(System.IO());
+    static Scanner src = new Scanner(System.in);
     public static void main(String[] args) {
         Bejegyzes a1 = new Bejegyzes("Tanárok", "Github commitolni kell srácok");
         Bejegyzes a2 = new Bejegyzes("Pénztáros", "Négyszáz és húsz forintot szeretnék elkérni");
@@ -17,5 +17,29 @@ public class Main {
         Bejegyzesek.add(a2);
         System.out.println(a1);
         System.out.println(a2);
+
+        BekerosFeladat();
+        Ellenőrzés();
+
     }
+
+    public static void BekerosFeladat() {
+        System.out.print("Írja be hány bejegyzést szeretne hozzáadni: ");
+        int hanyatSzeretne = Integer.parseInt(src.nextLine());
+        for (int i = 0; i < hanyatSzeretne; i++) {
+            System.out.println("Írja be a szerzőt: ");
+            String segedSzerzo = src.nextLine();
+            System.out.println("Írja be a tartalmat: ");
+            String segedTartalom = src.nextLine();
+            Bejegyzes e = new Bejegyzes(segedSzerzo, segedTartalom);
+            Bejegyzesek.add(e);
+        }
+    }
+
+    public static void Ellenőrzés() {
+        for (int i = 0; i < Bejegyzesek.size(); i++){
+            System.out.println(Bejegyzesek.get(i));
+        }
+    }
+
 }

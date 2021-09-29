55c1a5ff493bf5081a269a022689e9c7246e1d97
kekesibence <bence.kekesi@gmail.com>
2021-09-29 15:06:15 +0200

    toString metódus létrezhozása

--- Dolgozat09-29/src/hu/petrik/Bejegyzes.java
+++ Dolgozat09-29/src/hu/petrik/Bejegyzes.java
@@ -5,12 +5,22 @@ import java.time.LocalDateTime;
 public class Bejegyzes {
     private String szerzo;
     private String tartalom;
-    private Integer likeok = 0;
+    private Integer likeok;
     private LocalDateTime letrejott;
     private LocalDateTime szerkesztve;
 
     public Bejegyzes(String szerzo, String tartalom) {
         this.szerzo = szerzo;
         this.tartalom = tartalom;
+        this.likeok =  0;
+        this.letrejott = LocalDateTime.now();
+        this.szerkesztve = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return szerzo  + likeok + letrejott + '\''
+                + "Szerkesztve: " + szerkesztve + '\''
+                 + tartalom;
     }
 }

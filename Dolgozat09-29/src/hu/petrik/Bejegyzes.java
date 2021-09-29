39a29f5786d817e36f3a2b0f2e42df216eaf02e9
kekesibence <bence.kekesi@gmail.com>
2021-09-29 14:59:37 +0200

    Bejegyzes osztály változóinak létrehozása

--- Dolgozat09-29/src/hu/petrik/Bejegyzes.java
+++ Dolgozat09-29/src/hu/petrik/Bejegyzes.java
@@ -1,4 +1,16 @@
 package hu.petrik;
 
+import java.time.LocalDateTime;
+
 public class Bejegyzes {
+    private String szerzo;
+    private String tartalom;
+    private Integer likeok = 0;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+    public Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+    }
 }
